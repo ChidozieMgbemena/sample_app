1e11b5954ac401fcef2aed2d6c8f9b72007eabd4
Daniel Ugwa <akachiugwa@yahoo.com>
2016-02-05 20:41:17 +0000

    things working in chap 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed  
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
  
   before_save { self.email = email.downcase }  
   before_create :create_remember_token
@@ -13,10 +17,10 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
-  #def feed
+  def feed
     # This is preliminary. See "Following users" for the full implementation.
-   # Micropost.where("user_id = ?", id)
-  #end
+    Micropost.where("user_id = ?", id)
+  end
 
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
