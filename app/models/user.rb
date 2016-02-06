57b0d118b138d65fc5b40c491d5543b2fd8f89cd
Daniel Ugwa <akachiugwa@yahoo.com>
2016-02-06 12:04:26 +0000

    fixed conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 
   def feed
     # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ", id)
+    Micropost.where("user_id = ?", id)
   end
 
   def following?(other_user)
