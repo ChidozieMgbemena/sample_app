790973a69378508a19d2dc0ead7ec72827d97b31
akachi ugwa <akachiugwa@yahoo.com>
2016-02-10 11:10:11 +0000

    got database working after deleting vm

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema.define(version: 20160205193101) do
   enable_extension "plpgsql"
 
   create_table "microposts", force: :cascade do |t|
-    t.string   "content",    limit: 255
+    t.string   "content"
     t.integer  "user_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
@@ -37,13 +37,13 @@ ActiveRecord::Schema.define(version: 20160205193101) do
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "email",           limit: 255
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.string   "password_digest", limit: 255
-    t.string   "remember_token",  limit: 255
-    t.boolean  "admin",                       default: false
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
